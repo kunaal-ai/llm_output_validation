89859e48df61d22546e4f80703329671d9f7ccd9
Kunaal Thanik <kunaal@gmx.com>
2025-06-20 23:28:37 -0500

    re-strucuring

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+python -m pytest tests/test_diabetes.py --html=reports/test_report.html --self-contained-html
 
-python -m pytest test_diabetes.py --html=test_report.html --self-contained-html
+rm -r __pycache__/ .pytest_cache/
 
 exit 0
 
